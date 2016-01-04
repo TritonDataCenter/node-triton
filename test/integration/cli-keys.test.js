fb80f734f6ca02a474570e1ec5d5db4ce0071004
Marsell Kukuljevic <marsell@joyent.com>
2016-01-05 07:15:38 +1100

    clistyle: fix path problem with keys tests.

--- test/integration/cli-keys.test.js
+++ test/integration/cli-keys.test.js
@@ -20,7 +20,7 @@ var backoff = require('backoff');
 
 // --- Globals
 
-var KEY_PATH = 'data/id_rsa.pub';
+var KEY_PATH = __dirname + '/data/id_rsa.pub';
 var KEY_SIG  = '66:ca:1c:09:75:99:35:69:be:91:08:25:03:c0:17:c0';
 var KEY_EMAIL = 'test@localhost.local';
 var MAX_CHECK_KEY_TRIES = 10;
